d312dfcc9a504a840b2c16bba245eae5ff64b800
supersp39 <supriyapramanik3@gmai.com>
2023-06-02 14:22:21 +0530

    test

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,6 +49,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //todo:
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
